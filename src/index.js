bfcd3508154050c0c01a92633383e5c691cad04c
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-02-12 18:17:11 +0100

    particles!

--- src/index.js
+++ src/index.js
@@ -20,23 +20,41 @@ function start(image) {
             var b = img.data[c++];
             var a = img.data[c++];
             if (a > 0) {
-                particles.push({x: x, y: y, a: a});
+                particles.push({
+                    x: x, y: y, a: a,
+                    rx: Math.random() - 0.5, ry: Math.random() - 0.5
+                });
             }
         }
     }
     
+    function f(t, duration, cutoff) {
+        const rt = (t % duration) / duration;
+        if (rt < cutoff) {
+            return rt / cutoff;
+        }
+        return 0;
+    }
+
     function animate(t) {
         const backbuffer = new ImageData(canvas.width, canvas.height);
         const cx = (backbuffer.width - img.width) / 2;
         const cy = (backbuffer.height - img.height) / 2;
+        const duration = 4000;
         for (var i = 0; i < particles.length; i++) {
-            const x = cx + particles[i].x;
-            const y = cy + particles[i].y;
+            const px = particles[i].x;
+            const py = particles[i].y;
+            const rx = particles[i].rx;
+            const ry = particles[i].ry;
+            const phase = -px * 10;
+            const alpha = Math.PI * f(t + phase, duration, 0.3);
+            const x = cx + px + Math.sin(alpha) * 180 * rx;
+            const y = cy + py + Math.sin(alpha) * 400 * ry;
             const a = particles[i].a;
-            const offset = 4 * (x + y * backbuffer.width);
-            backbuffer.data[offset + 0] = 0;
-            backbuffer.data[offset + 1] = 0;
-            backbuffer.data[offset + 2] = 0;
+            const offset = 4 * (Math.trunc(x) + Math.trunc(y) * backbuffer.width);
+            backbuffer.data[offset + 0] = 0xd3;
+            backbuffer.data[offset + 1] = 0x17;
+            backbuffer.data[offset + 2] = 0xe8;
             backbuffer.data[offset + 3] = a;
             
         }
