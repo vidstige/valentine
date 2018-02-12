7ddc5eea8d71e1962cf1c34c99be3faf7916b309
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-02-12 17:07:59 +0100

    load and draw png

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 require("../style.css");
-console.log('hi');
+const heart = require('../static/heart.png');
+
+function main() {
+    var c = document.getElementById("target");
+    var ctx = c.getContext("2d");
+    var img = new Image();
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0);
+    };
+    img.src = heart;
+}
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    main();
+});
+
