a373b7ef843d3d34ff88017bd4033238103585b4
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-02-14 08:13:35 +0100

    refactor: extract function

--- src/index.js
+++ src/index.js
@@ -7,14 +7,11 @@ function color2obj(color) {
     return {r: rgb[0], g: rgb[1], b: rgb[2]};
 }
 
-function start(image) {
-    const canvas = document.getElementById("target");
-    const ctx = canvas.getContext("2d");
-    
+function getParticles(ctx, image) {
     // Get image data
     ctx.drawImage(image, 0, 0);
     const img = ctx.getImageData(0, 0, image.width, image.height);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, image.width, image.height);
 
     // Convert to coordinates
     const particles = [];
@@ -26,16 +23,25 @@ function start(image) {
             var b = img.data[c++];
             var a = img.data[c++];
             if (a > 0) {
-                particles.push({
-                    x: x, y: y, a: a,
-                    rx: Math.random() - 0.5, ry: Math.random() - 0.5
-                });
+                particles.push({x: x, y: y, a: a});
             }
         }
     }
+    return particles;
+}
+
+function start(image) {
+    const canvas = document.getElementById("target");
+    const ctx = canvas.getContext("2d");
 
     const foreground = color2obj(getComputedStyle(canvas).color);
     
+    const particles = getParticles(ctx, image);
+    for (var i = 0; i < particles.length; i++) {
+        particles[i].rx = Math.random() - 0.5;
+        particles[i].ry = Math.random() - 0.5;
+    }
+
     function f(t, duration, cutoff) {
         const rt = (t % duration) / duration;
         if (rt < cutoff) {
@@ -46,8 +52,8 @@ function start(image) {
 
     function animate(t) {
         const backbuffer = new ImageData(canvas.width, canvas.height);
-        const cx = (backbuffer.width - img.width) / 2;
-        const cy = (backbuffer.height - img.height) / 2;
+        const cx = (backbuffer.width - image.width) / 2;
+        const cy = (backbuffer.height - image.height) / 2;
         const duration = 4000;
         for (var i = 0; i < particles.length; i++) {
             const px = particles[i].x;
