8cf752c2c15caaad8310fc63d32d59875cc68cff
Samuel Carlsson <samuel.carlsson@volumental.com>
2018-02-14 11:07:39 +0100

    morph logos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require("../style.css");
-//const heart = require('../static/heart.png');
+const heart = require('../static/heart.png');
 const volumental = require('../static/volumental.png');
 
 function color2obj(color) {
@@ -30,7 +30,35 @@ function getParticles(ctx, image) {
     return particles;
 }
 
-function start(image) {
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+// Returns an array of size a, that contains all
+// indices 0..b-1 in random order. Some indices may
+// appear more than once if a>b
+function getRandomMapping(a, b) {
+    if (b > a) {
+        return null;
+    }
+    var taken = new Set();
+    var m = [];
+    for (var i = 0; i < a; i++) {
+        var s = getRandomInt(b);
+        while (i < b && taken.has(s)) {
+            s = getRandomInt(b);
+        }
+        m[i] = s
+        taken.add(s);
+    }
+    return m;
+}
+
+function lerp(a, b, t) {
+    return a * t + b * (1-t);
+}
+
+function start(image, image2) {
     const canvas = document.getElementById("target");
     const ctx = canvas.getContext("2d");
 
@@ -41,6 +69,11 @@ function start(image) {
         particles[i].rx = Math.random() - 0.5;
         particles[i].ry = Math.random() - 0.5;
     }
+    const particles2 = getParticles(ctx, image2);
+    console.log(particles.length, particles2.length);
+    
+    const mapping = getRandomMapping(particles.length, particles2.length);
+    console.log(mapping);
 
     function f(t, duration, cutoff) {
         const rt = (t % duration) / duration;
@@ -55,16 +88,22 @@ function start(image) {
         const cx = (backbuffer.width - image.width) / 2;
         const cy = (backbuffer.height - image.height) / 2;
         const duration = 4000;
+        
         for (var i = 0; i < particles.length; i++) {
             const px = particles[i].x;
             const py = particles[i].y;
             const rx = particles[i].rx;
             const ry = particles[i].ry;
             const phase = -px * 10;
-            const alpha = Math.PI * f(t + phase, duration, 0.3);
-            const x = cx + px + Math.sin(alpha) * 180 * rx;
-            const y = cy + py + Math.sin(alpha) * 400 * ry;
-            const a = particles[i].a;
+
+            const j = mapping[i];
+            const px2 = particles2[j].x;
+            const py2 = particles2[j].y;
+            const k = (Math.sin(t / 1000) + 1)/2;
+            const x = cx + lerp(px2, px, k);
+            const y = cy + lerp(py2, py, k);
+            const a = lerp(particles2[j].a, particles[i].a, k);
+
             const offset = 4 * (Math.trunc(x) + Math.trunc(y) * backbuffer.width);
             backbuffer.data[offset + 0] = foreground.r;
             backbuffer.data[offset + 1] = foreground.g;
@@ -79,12 +118,20 @@ function start(image) {
 }
 
 function load() {
-    const images = [volumental];
+    const images = [heart, volumental];
+    var done = [];
     for (var i = 0; i < images.length; i++) {
         const img = new Image();
-        img.onload = function() { start(this); };
+        img.onload = function() {
+            done.push(this);
+            if (done.length == images.length) {
+                console.log("all loaded");
+                start.apply(null, done);
+            }            
+        };
         img.src = images[i];
     }
 }
 
 document.addEventListener("DOMContentLoaded", load);
+
