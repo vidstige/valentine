7c781791f357ff424306cead7ef35b74932496de
Samuel Carlsson <samuel.carlsson@volumental.com>
2021-03-07 13:23:24 +0100

    refactor: project takes array2f

--- src/solver.c
+++ src/solver.c
@@ -16,7 +16,7 @@ void add_source(const array2f *array, const array2f *source, float dt )
 	}
 }
 
-void set_bnd ( int N, int b, float * x )
+void set_bnd(int N, int b, float * x)
 {
 	int i;
 
@@ -36,7 +36,7 @@ void lin_solve ( int N, int b, float * x, float * x0, float a, float c )
 {
 	int i, j, k;
 
-	for ( k=0 ; k<20 ; k++ ) {
+	for (size_t k = 0; k < 20; k++) {
 		FOR_EACH_CELL
 			x[IX(i,j)] = (x0[IX(i,j)] + a*(x[IX(i-1,j)]+x[IX(i+1,j)]+x[IX(i,j-1)]+x[IX(i,j+1)]))/c;
 		END_FOR
@@ -45,7 +45,7 @@ void lin_solve ( int N, int b, float * x, float * x0, float a, float c )
 
 void diffuse(int N, int b, const array2f *x, const array2f *x0, float diff, float dt )
 {
-	float a=dt * diff * N * N;
+	float a = dt * diff * N * N;
 	lin_solve( N, b, x->buffer, x0->buffer, a, 1 + 4 * a);
 	set_bnd(N, b, x->buffer);
 }
@@ -71,24 +71,24 @@ void advect ( int N, int b, float * d, float * d0, float * u, float * v, float d
 	set_bnd ( N, b, d );
 }
 
-void project ( int N, float * u, float * v, float * p, float * div )
+void project(int N, const array2f *u, const array2f *v, const array2f *p, const array2f *div)
 {
 	int i, j;
 
 	FOR_EACH_CELL
-		div[IX(i,j)] = -0.5f*(u[IX(i+1,j)]-u[IX(i-1,j)]+v[IX(i,j+1)]-v[IX(i,j-1)])/N;
-		p[IX(i,j)] = 0;
+		div->buffer[IX(i,j)] = -0.5f*(u->buffer[IX(i+1,j)]-u->buffer[IX(i-1,j)]+v->buffer[IX(i,j+1)]-v->buffer[IX(i,j-1)])/N;
+		p->buffer[IX(i,j)] = 0;
 	END_FOR	
-	set_bnd ( N, 0, div ); set_bnd ( N, 0, p );
+	set_bnd ( N, 0, div->buffer ); set_bnd ( N, 0, p->buffer );
 
-	lin_solve ( N, 0, p, div, 1, 4 );
-	set_bnd ( N, 0, p );
+	lin_solve ( N, 0, p->buffer, div->buffer, 1, 4 );
+	set_bnd ( N, 0, p->buffer );
 
 	FOR_EACH_CELL
-		u[IX(i,j)] -= 0.5f*N*(p[IX(i+1,j)]-p[IX(i-1,j)]);
-		v[IX(i,j)] -= 0.5f*N*(p[IX(i,j+1)]-p[IX(i,j-1)]);
+		u->buffer[IX(i,j)] -= 0.5f*N*(p->buffer[IX(i+1,j)]-p->buffer[IX(i-1,j)]);
+		v->buffer[IX(i,j)] -= 0.5f*N*(p->buffer[IX(i,j+1)]-p->buffer[IX(i,j-1)]);
 	END_FOR
-	set_bnd ( N, 1, u ); set_bnd ( N, 2, v );
+	set_bnd ( N, 1, u->buffer ); set_bnd ( N, 2, v->buffer );
 }
 
 void density_step(size_t N, array2f *x, array2f *x0, array2f *u, array2f *v, float diff, float dt)
@@ -101,11 +101,11 @@ void density_step(size_t N, array2f *x, array2f *x0, array2f *u, array2f *v, flo
 void velocity_step(size_t N, array2f *u, array2f *v, array2f *u0, array2f *v0, float visc, float dt)
 {
 	add_source(u, u0, dt); add_source(v, v0, dt);
-	SWAP ( u0, u ); diffuse(N, 1, u, u0, visc, dt );
-	SWAP ( v0, v ); diffuse(N, 2, v, v0, visc, dt );
-	project ( N, u->buffer, v->buffer, u0->buffer, v0->buffer );
+	SWAP ( u0, u ); diffuse(N, 1, u, u0, visc, dt);
+	SWAP ( v0, v ); diffuse(N, 2, v, v0, visc, dt);
+	project ( N, u, v, u0, v0 );
 	SWAP ( u0, u ); SWAP ( v0, v );
-	advect ( N, 1, u->buffer, u0->buffer, u0->buffer, v0->buffer, dt );
-	advect ( N, 2, v->buffer, v0->buffer, u0->buffer, v0->buffer, dt );
-	project ( N, u->buffer, v->buffer, u0->buffer, v0->buffer );
+	advect ( N, 1, u->buffer, u0->buffer, u0->buffer, v0->buffer, dt);
+	advect ( N, 2, v->buffer, v0->buffer, u0->buffer, v0->buffer, dt);
+	project ( N, u, v, u0, v0 );
 }
