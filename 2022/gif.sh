3bb5bfe42247af9323f1087781f15a27a300095b
Samuel Carlsson <samuel.carlsson@volumental.com>
2022-02-04 16:56:13 +0100

    hq gifs

--- 2022/gif.sh
+++ 2022/gif.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 RESOLUTION=${RESOLUTION:-720x720} 
-ffmpeg -v warning -f rawvideo -pixel_format rgb32 -framerate 30 -video_size $RESOLUTION -i - $1
+FPS=30
+ffmpeg -hide_banner -f rawvideo -pixel_format rgb32 -framerate $FPS -video_size $RESOLUTION -i - -metadata comment="vidstige" -vf "fps=$FPS,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" $1
