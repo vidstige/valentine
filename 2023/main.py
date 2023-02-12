69031a76063b0d11ab60684141c14b9f3c2fca8b
Samuel Carlsson <samuel.carlsson@volumental.com>
2023-02-12 16:49:08 +0100

    hacking

--- 2023/main.py
+++ 2023/main.py
@@ -1,16 +1,21 @@
 from collections import deque
 from math import e, tau
 import sys
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
 import cairo
 import numpy as np
+from numpy.lib.function_base import meshgrid
 from svgpathtools import parse_path, Path, path_encloses_pt
 
 from transform import transform
+from perlin import generate_perlin_noise_2d
 
 
 HEART = parse_path("M348.151,54.514c-19.883-19.884-46.315-30.826-74.435-30.826c-28.124,0-54.559,10.942-74.449,30.826l-9.798,9.8l-9.798-9.8 c-19.884-19.884-46.325-30.826-74.443-30.826c-28.117,0-54.56,10.942-74.442,30.826c-41.049,41.053-41.049,107.848,0,148.885 l147.09,147.091c2.405,2.414,5.399,3.892,8.527,4.461c1.049,0.207,2.104,0.303,3.161,0.303c4.161,0,8.329-1.587,11.498-4.764 l147.09-147.091C389.203,162.362,389.203,95.567,348.151,54.514z")
+
+
+Color = Tuple[float, float, float]
 
 
 def create_sdf(path: Path, size: Tuple[float, float], resolution: Tuple[int, int], n: int):
@@ -62,7 +67,7 @@ class Dot:
     def __init__(self, position: complex, velocity: complex):
         self.position = position
         self.velocity = velocity
-        self.trace = deque()
+        self.trace = deque()  # type: deque[complex]
 
     def update(self, acceleration: complex, dt: float) -> None:
         self.trace.appendleft(self.position)
@@ -106,18 +111,20 @@ def is_inside_path(path: Path, p: complex) -> bool:
     return path_encloses_pt(p, outside, path)
 
 
-def clear(target: cairo.ImageSurface) -> None:
+def clear(target: cairo.ImageSurface, color: Color) -> None:
     ctx = cairo.Context(target)
-    ctx.set_operator(cairo.OPERATOR_CLEAR)
+    ctx.set_source_rgb(*color)
+    ctx.set_operator(cairo.OPERATOR_SOURCE)
     ctx.paint()
 
 
-def draw(target: cairo.ImageSurface, dots: List[Dot]) -> None:
+def draw(target: cairo.ImageSurface, dots: List[Dot], color: Color) -> None:
     ctx = cairo.Context(target)
     ctx.set_source_rgb(1, 1, 1)
     
     #r = 3
     ctx.set_line_width(1)
+    ctx.set_source_rgb(*color)
     for dot in dots:
         for p in dot.trace:
             ctx.line_to(p.real, p.imag)
@@ -133,6 +140,21 @@ def spawn(path: Path, t: float) -> Tuple[complex, complex]:
         v = -path.derivative(t) * e ** (tau * -0.10j)
     return path.point(t), 50 * v / abs(v)
 
+gradient_cache = {}  # type: Dict[int, Tuple[np.ndarray, np.ndarray]]
+def gradient(field: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    if id(field) in gradient_cache:
+        return gradient_cache[id(field)]
+    dy, dx = np.gradient(field)
+    gradient_cache[id(field)] = (dx, dy)
+    return dx, dy
+
+
+def gradient_at(field: np.ndarray, size: Tuple[float, float], p: complex) -> complex:
+    """computes the gradient of a fields as a complex (x + yi) coordinate"""
+    # TODO: cache dx, dy
+    dx, dy = gradient(field)
+    return at(dx, size, p) + 1j * at(dy, size, p)
+
 
 def main():
     path = transform(HEART, 0.5, 100 + 100j)
@@ -143,29 +165,35 @@ def main():
     size = (400, 400)
     resolution = (400, 400)
 
-    dots = [Dot(*spawn(path, t)) for t in np.random.random(N)]
-    #dots = [random_dot(resolution, 100) for _ in range(N)]
+    #dots = [Dot(*spawn(path, t)) for t in np.random.random(N)]
+    dots = [random_dot(resolution, 100) for _ in range(N)]
     
     sdf = create_sdf(path, (400, 400), resolution, n=100)
-    inside = create_inside_lookup(path, size, (50, 50))
-    dy, dx = np.gradient(G * sdf)
+    #inside = create_inside_lookup(path, size, (50, 50))
+    velocity_field = G * sdf
 
     surface = cairo.ImageSurface(cairo.Format.ARGB32, *resolution)
     
     for t in np.arange(0, 60, dt):
         # step
         for dot in dots:
-            dv = at(dx, size, dot.position) + 1j * at(dy, size, dot.position)
+            dv = gradient_at(velocity_field, size, dot.position)
+            #dv = at(dx, size, dot.position) + 1j * at(dy, size, dot.position)
             dot.update(20 * -dv, dt)
             
-            if not is_inside(resolution, dot.position) or at(inside, size, dot.position):
+            #if not is_inside(resolution, dot.position) or at(inside, size, dot.position):
+            if not is_inside(resolution, dot.position):
                 dot.retract()
                 if not dot.trace:
                     dot.respawn(*spawn(path, np.random.random()))
         
-        clear(surface)
-        draw(surface, dots)
+        clear(surface, (1, 1, 1))
+        draw(surface, dots, (0, 0, 0))
         
+        #rng = np.random.Generator(np.random.PCG64(1337))
+        #noise = generate_perlin_noise_2d(resolution, (5, 5), rng)
+        #sys.stdout.buffer.write(encode_frame(from_gray(noise * 0.5 + 0.5)))
+
         sys.stdout.buffer.write(surface.get_data())
         #sys.stdout.buffer.write(encode_frame(from_gray(frame)))
         #sys.stdout.buffer.write(encode_frame(from_gray(autoscale(sdf))))
