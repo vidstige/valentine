3ef7f95032ed51e8e058ce62c252c4820d1e209b
Samuel Carlsson <samuel.carlsson@gmail.com>
2024-02-07 21:19:08 +0100

    polygon clipping
    Weiler–Atherton

--- 2024/main.py
+++ 2024/main.py
@@ -2,7 +2,7 @@ import os
 import sys
 import random
 import math
-from typing import BinaryIO, Callable, Iterable, List, Optional, Tuple
+from typing import BinaryIO, Callable, Iterable, List, Optional, Tuple, TypeVar
 
 import cairo
 from PIL import Image, ImageFilter
@@ -44,8 +44,14 @@ def lerp(a: Point, b: Point, t: float) -> Point:
 
 
 LineSegment = Tuple[Point, Point]
-Intersection = Tuple[Point, float]
-def intersection(ls0: LineSegment, ls1: LineSegment) -> Optional[Intersection]:
+
+def cross(a: Point, b: Point) -> float:
+    ax, ay = a
+    bx, by = b
+    return ax * by - ay * bx
+
+
+def intersection(ls0: LineSegment, ls1: LineSegment) -> Optional[Point]:
     (x1, y1), (x2, y2) = ls0
     (x3, y3), (x4, y4) = ls1
     d = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
@@ -54,28 +60,79 @@ def intersection(ls0: LineSegment, ls1: LineSegment) -> Optional[Intersection]:
         return None
     t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / d
     u = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3))  / d
+    # TODO: Allow cutting line to extend indefinataly
     if t < 0 or t >= 1 or u < 0 or u >= 1:
         # intersection outside linesegments
         return None
     # intersection found - compute intersection point using t (u could also be used)
-    p = lerp(*ls0, t)
-    return p, d
+    return lerp(*ls0, t)
 
 
 Polygon = List[Point]
-def linesegments(polygon: Polygon) -> Iterable[LineSegment]:
-    """Returns the line segments of the polygon"""
-    yield from (ls for ls in zip(polygon, polygon[1:]))
-    yield polygon[-1], polygon[0]  # close polygon
+
+from itertools import chain, tee
+
+T = TypeVar('T')
+def sides(iterable: Iterable[T]) -> Iterable[Tuple[T, T]]:
+    a, b = tee(iterable)
+    first = next(b, None)
+    return zip(a, chain(b, (first,)))
 
 
+import math
 def split(polygon: Polygon, linesegment: LineSegment) -> Tuple[List[Polygon], List[Polygon]]:
-    # find intersections and wether they are "entering" or "exiting"
-    for ls in linesegments(polygon):
-        print(intersection(linesegment, ls), file=sys.stderr)
-    left = []
-    right = []
-    return left, right
+    (ax, ay), (bx, by) = linesegment
+    # linesegment vector (a -> b)
+    lsv = bx - ax, by - ay
+
+    # compute signed distance from line segment to all vertices in polygon
+    signed_distances = [cross(lsv, (x - ax, y - ay)) for x, y in polygon]
+
+    # all vertices are to the left of the line segment
+    if all(d < 0 for d in signed_distances):
+        return [polygon], []
+    # all vertices are to the right of the line segment
+    if all(d > 0 for d in signed_distances):
+        return [], [polygon]
+
+    # compute intersections (using signed distance)
+    vertices = []
+    for (da, pa), (db, pb) in sides(zip(signed_distances, polygon)):
+        # always add "first" point of the segment
+        vertices.append((pa, False, False))  # not entering nor exiting
+
+        # check if segment enters or exits clip area (across line segment)
+        entering = da < 0 and db > 0
+        exiting = da > 0 and db < 0
+        if entering or exiting:
+            # find intersection point i
+            i = intersection(linesegment, (pa, pb))
+            # there should always be an intersection point between
+            # an inside and outside point
+            assert i is not None
+            vertices.append((i, entering, exiting))
+    
+    # find index of first entering vertex
+    ei = [entering for _, entering, _ in vertices].index(True)
+    
+    current = []
+    inside, outside = [current], []
+    # go through all vertices, starting with first entering one
+    for i in range(len(vertices)):
+        j = (ei + i) % len(vertices)
+        p, entering, exiting = vertices[j]
+        current.append(p)
+        if exiting:
+            current = [p]  # create new polygon
+            outside.append(current)
+        if entering:
+            current = [p]  # create new polygon
+            inside.append(current)
+    # move the single-vertex polygon to the end of the last outside polygon
+    tmp = inside.pop(0)
+    outside[-1].extend(tmp)
+
+    return inside, outside
 
 
 def draw_polygon(ctx: cairo.Context, polygon: Polygon) -> None:
@@ -96,12 +153,6 @@ def draw(target: cairo.ImageSurface, t: float) -> None:
     ctx.line_to(*line[1])
     ctx.stroke()
 
-    # draw original polyon
-    ctx.set_source_rgb(0.6, 0.6, 0.6)
-    ctx.set_dash([10, 5])
-    draw_polygon(ctx, polygon)
-    ctx.stroke()
-
     left_polygons, right_polygons = split(polygon, line)
     ctx.set_fill_rule(cairo.FILL_RULE_EVEN_ODD)
     ctx.set_source_rgb(0.7, 0.1, 0.1)
@@ -112,6 +163,12 @@ def draw(target: cairo.ImageSurface, t: float) -> None:
     for p in right_polygons:
         draw_polygon(ctx, p)
     ctx.fill()
+    
+    # draw original polyon
+    ctx.set_source_rgb(0.6, 0.6, 0.6)
+    ctx.set_dash([10, 5])
+    draw_polygon(ctx, polygon)
+    ctx.stroke()
 
 
 def animate(f: BinaryIO, draw: Callable[[cairo.ImageSurface, float], None], dt: float):
