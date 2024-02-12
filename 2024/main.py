22619e8cf1c2cd6ccc0a318fc2a07378b0fec86e
Samuel Carlsson <samuel.carlsson@gmail.com>
2024-02-12 15:44:14 +0100

    prepare to animate again

--- 2024/main.py
+++ 2024/main.py
@@ -3,19 +3,18 @@ import os
 import sys
 import math
 from textwrap import wrap
-from typing import BinaryIO, Callable, Tuple
+from typing import BinaryIO, List, Tuple
 
 import cairo
 from PIL import Image
-from shapely import Polygon, transform
+from shapely import Polygon, MultiPolygon, transform
 
-from valentine.resolution import parse_resolution
+from valentine.resolution import Resolution, parse_resolution
 import valentine.zoom
 import valentine.svg
 from valentine import tony
 
 TAU = 2 * math.pi
-RESOLUTION = parse_resolution(os.environ.get('RESOLUTION', '720x720'))
 
 
 def parse_color(color: str) -> Tuple[float, float, float]:
@@ -43,59 +42,40 @@ def draw_polygon(ctx: cairo.Context, polygon: Polygon) -> None:
     ctx.close_path()
 
 
-def animate(f: BinaryIO, draw: Callable[[cairo.ImageSurface, float], None], dt: float):
-    width, height = RESOLUTION
+def draw(surface: cairo.ImageSurface, pieces: List[Polygon], t: float) -> None:
+    pass
+
+
+def load_svg(path: str, resolution: Resolution) -> MultiPolygon:
+    """Loads path, converts to polygon(s) and translates/scales to resolution"""
+    polygons = valentine.svg.load(path)
+    zoom = valentine.zoom.zoom_to(polygons.bounds, resolution, padding=32)
+    return transform(polygons, zoom.transform)
+
+
+def animate(f: BinaryIO, resolution: Resolution, dt: float):        
+    #polygons = load_svg('volumental.svg')
+    polygons = load_svg('heart.svg', resolution)
+
+    # cut polygons
+    lines = list(tony.grid(resolution, (7, 7), value=0.4))
+    pieces = tony.cut(polygons, lines)
+
+    width, height = resolution
     surface = cairo.ImageSurface(cairo.Format.ARGB32, width, height)
-    
+
     t = 0
     while t < 1:
         clear(surface)
-        draw(surface, t)
+        draw(surface, pieces, t)
         f.write(surface.get_data())
         t += dt
 
 
 def main():
-    #polygons = valentine.svg.load('volumental.svg')
-    polygons = valentine.svg.load('heart.svg')
-    zoom = valentine.zoom.zoom_to(polygons.bounds, RESOLUTION, padding=32)
-    polygons = transform(polygons, zoom.transform)
+    resolution = parse_resolution(os.environ.get('RESOLUTION', '720x720'))
+    animate(sys.stdout.buffer, resolution, dt=0.008)
 
-    lines = list(tony.grid(RESOLUTION, (7, 7), value=0.4))
-
-    width, height = RESOLUTION
-    surface = cairo.ImageSurface(cairo.Format.ARGB32, width, height)    
-    
-    # cut polygons
-    pieces = tony.cut(polygons, lines)
-
-    clear(surface)    
-
-    ctx = cairo.Context(surface)
-    ctx.set_source_rgb(0.4, 0.4, 0.4)
-    ctx.set_dash([10, 5])
-    for line in lines:
-        ctx.new_path()
-        for x, y in line.coords:
-            ctx.line_to(x, y)
-        ctx.stroke()
-
-    colors = [parse_color(s) for s in [
-        '#FF9AA2',
-        '#FFB7B2',
-        '#FFDAC1',
-        '#E2F0CB',
-        '#B5EAD7',
-        '#C7CEEA',
-    ]]
-    for polygon, color in zip(pieces.geoms, cycle(colors)):
-        ctx.set_source_rgb(*color)
-        draw_polygon(ctx, polygon)
-        ctx.fill()
-    
-    surface.write_to_png('debug.png')
-
-    #animate(sys.stdout.buffer, draw, dt=0.008)
 
 if __name__ == "__main__":
     main()
