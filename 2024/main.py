d7873a9ce14d82a0c12d13320d0680c3f0ee73c2
Samuel Carlsson <samuel.carlsson@gmail.com>
2024-02-13 20:50:24 +0100

    commented out motionblur (slow)

--- 2024/main.py
+++ 2024/main.py
@@ -1,12 +1,14 @@
+from collections import deque
 from itertools import count, cycle
 import os
 import sys
 import math
 import random
 from textwrap import wrap
-from typing import BinaryIO, List, Tuple
+from typing import BinaryIO, Iterable, List, Tuple
 
 import cairo
+import numpy as np
 from PIL import Image
 from shapely import Polygon, MultiPolygon, transform
 
@@ -88,6 +90,20 @@ def load_svg(path: str, resolution: Resolution) -> MultiPolygon:
     return transform(polygons, zoom.transform)
 
 
+def as_array(surface: cairo.ImageSurface) -> np.ndarray:
+    width, height = surface.get_width(), surface.get_height()
+    return np.ndarray(
+        shape=(height, width, 4),
+        dtype=np.uint8,
+        buffer=surface.get_data(),
+    ).copy()
+
+
+def motion_blur(frames: Iterable[np.ndarray]) -> np.ndarray:
+    array = np.stack([frame.astype(np.uint32) for frame in frames], axis=-1)
+    return np.mean(array, axis=-1).astype(np.uint8)
+
+
 def animate(f: BinaryIO, resolution: Resolution, dt: float):        
     # cut polygons, first create templates
     random.seed(1337)
@@ -101,8 +117,8 @@ def animate(f: BinaryIO, resolution: Resolution, dt: float):
     timeline = Timeline()
     _, height = resolution
     timeline.add('heart.y', TweenSequence([
-        Constant(-height, duration=4.0),
-        Linear(-height, 0, duration=0.20),
+        Constant(-height, duration=3.0),
+        Linear(-height, 0, duration=0.50),
         Constant(0, duration=3.0),
         Linear(0, height, duration=0.8),
         Constant(height, duration=2.0),
@@ -110,7 +126,7 @@ def animate(f: BinaryIO, resolution: Resolution, dt: float):
     heart_duration = timeline.tag('heart.y').duration()
     timeline.add('logo.y', TweenSequence([
         Constant(-height, duration=0.75 * heart_duration),
-        Linear(-height, 0, duration=0.20),
+        Linear(-height, 0, duration=0.50),
         Constant(0, duration=3.0),
         Linear(0, height, duration=0.8),
         Constant(height, duration=0.5),
@@ -126,14 +142,21 @@ def animate(f: BinaryIO, resolution: Resolution, dt: float):
     duration = timeline.duration()
     while t < duration:
         clear(surface)
+        #frames = []
+        #for i in range(8):
+        #    draw(surface, timeline, phases, logo, heart, t - i * dt * 0.2)
+        #    frames.append(as_array(surface))
+        #buffer = motion_blur(frames).tobytes()
+        
         draw(surface, timeline, phases, logo, heart, t)
-        f.write(surface.get_data())
+        buffer = surface.get_data()
+        f.write(buffer)
         t += dt
 
 
 def main():
     resolution = parse_resolution(os.environ.get('RESOLUTION', '720x720'))
-    animate(sys.stdout.buffer, resolution, dt=1/40)
+    animate(sys.stdout.buffer, resolution, dt=1/60)
 
 
 if __name__ == "__main__":
